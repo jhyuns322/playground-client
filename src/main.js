ceff54b63e4121935828f6e709ed66332e4b8c16
jonghyun <jhyuns322@gmail.com>
2023-02-09 20:40:04 +0900

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import i18n from './i18n'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(i18n)
+app.use(store)
+app.use(router)
+.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>{{ $t('about.message') }}</h1>
+  </div>
+</template>
